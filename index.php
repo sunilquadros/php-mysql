cb7f8df5c1ed3e3beb5fee1369ed9f3a23da65ea
sunilquadros <sunilquadros@workstation.lab.example.com>
2017-06-27 15:28:08 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "172.30.251.114";
+$username = "ose";
+$password = "openshift";
+$dbname = "quotes"
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
