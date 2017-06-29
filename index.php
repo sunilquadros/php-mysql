29f42a57617dacb0700924800b8b2302666a3fe7
sunilquadros <sunilquadros@workstation.lab.example.com>
2017-06-29 07:07:21 -0400

    3rd update to repo

--- index.php
+++ index.php
@@ -13,4 +13,17 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 echo "Connected successfully";
+
+$sql = "SELECT id, msg from quote";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+   // output data of each row
+   while($row = $result->fetch_assoc()) {
+   echo "id: " . $row["id"]. " - Message: " . $row["msg"]. "<br>";
+   }
+} else {
+   echo "0 results";
+}
+$conn->close();
 ?>
